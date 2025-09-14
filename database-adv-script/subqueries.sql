03dab1317e0fdf4677c6ed0e8a0dbde0a49ebf11
Abraham Kwesi James <abrahamkwesijames@gmail.com>
2025-09-14 19:37:47 +0000

    Yayy

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,28 @@
+-- 1️⃣ Non-Correlated Subquery:
+-- Find all properties where the average rating is greater than 4.0
+SELECT 
+    property_id,
+    name AS property_name
+FROM properties
+WHERE property_id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+)
+ORDER BY property_id;
+
+
+-- 2️⃣ Correlated Subquery:
+-- Find users who have made more than 3 bookings
+SELECT 
+    user_id,
+    name AS user_name,
+    email
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3
+ORDER BY user_id;
